e5946f55c10ae6fc057077b8bc37c0fbdd6a1e65
noemiguapi0393-collab <noemi.guapi0393@utc.edu.ec>
2026-06-11 22:18:58 -0500

    Añadir metodo depositar_dinero y completar opcion 3

--- Trabajo-grupal-NG/src/whilepractica1/Whilepractica1.java
+++ Trabajo-grupal-NG/src/whilepractica1/Whilepractica1.java
@@ -7,117 +7,105 @@ package whilepractica1;
 import java.util.Scanner;
 
 /**
- *
  * @author Estudiante
  */
 public class Whilepractica1 {
-    //ponemos el saldo aqui afuera para que todos los metodos los visualicen 
-    
-    //y se actualicen de verdad 
      public static double saldo = 500;
      public static Scanner sc = new Scanner(System.in);
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        cajero_automatico();
     }
     
-
      public static void cajero_automatico(){
-        
-     
-
-         //algoritmo para simular cajero automatico
          int clave = 1234;
          int intentos = 3;
          while (intentos > 0 && intentos <=3){
-         System.out.println("cajero automatico");
-         System.out.println("------------------");
-         System.out.println("ingrese su tarjeta: ");
-         System.out.println("tarjeta detectada");
-         System.out.println("ingrese su clave");
-         int clave_ingresada = sc.nextInt();
-         int opcion;
-         int intentos2 = 3;
-         if (clave_ingresada == clave){
-            do {
-             //si la clave concide y|
-             System.out.println("bienvenido al cajero");
-             System.out.println("seleccione una opcion: ");
-             System.out.println("1.consultar saldo");
-             System.out.println("2.retirar dinero");
-             System.out.println("3.depositar dinero");
-             System.out.println("4.salir");
-             System.out.println("ingrese la opcion deseada");
-             
-              opcion = sc.nextInt();
-              
-             
-             switch (opcion){
-                 case 1://opcion consultar
-                
-                     System.out.println("consultar saldo");
-                     consultar_saldo();   
-                     break;
-                 case 2://opcion retirar
-                     
-                    
-                     retirar_saldo();
-                     
-                     break;
-                 case 3://opcion depositar
-                     System.out.println("depositar dinero");
-                     break;
-                 case 4://opcion salir 
-                     System.out.println("gracias por preferirnos");
-                     break;
-                 default:
-                     System.out.println("opcion incorrecta");
-                     intentos2 --;
-                    
-            }
-             
-            } while (opcion !=4 && intentos >0);
-                break;
+             System.out.println("cajero automatico");
+             System.out.println("------------------");
+             System.out.println("ingrese su tarjeta: ");
+             System.out.println("tarjeta detectada");
+             System.out.println("ingrese su clave");
+             int clave_ingresada = sc.nextInt();
+             int opcion;
+             int intentos2 = 3;
+             if (clave_ingresada == clave){
+                do {
+                 System.out.println("bienvenido al cajero");
+                 System.out.println("seleccione una opcion: ");
+                 System.out.println("1.consultar saldo");
+                 System.out.println("2.retirar dinero");
+                 System.out.println("3.depositar dinero");
+                 System.out.println("4.salir");
+                 System.out.println("ingrese la opcion deseada");
                  
-                   
-        
-         
+                 opcion = sc.nextInt();
+                  
+                 switch (opcion){
+                     case 1:
+                         System.out.println("consultar saldo");
+                         consultar_saldo();   
+                         break;
+                     case 2:
+                         retirar_saldo();
+                         break;
+                     case 3:
+                         // CAMBIO 1: Se llama al nuevo método para depositar
+                         depositar_dinero();
+                         break;
+                     case 4:
+                         System.out.println("gracias por preferirnos");
+                         break;
+                     default:
+                         System.out.println("opcion incorrecta");
+                         intentos2 --;
+                 }
+                 
+                } while (opcion !=4 && intentos >0);
+                    break;
+                     
                  }else{
-                         
-             System.out.println("clave incorrecta");
-             intentos --;
-             System.out.println("te quedan "+ intentos);
-         } 
-     }
+                 System.out.println("clave incorrecta");
+                 intentos --;
+                 System.out.println("te quedan "+ intentos);
+             } 
+         }
          if (intentos == 0){
              System.out.println("tarjeta bloqueada, haz superado los 3 intentos");
          }
      }
-     //consultar saldo
+     
      public static void consultar_saldo(){
          System.out.println("--------CONSULTAR SALDO --------");
          System.out.println("el saldo disponible es de "+ saldo);
      }
-     //retirar saldo 
+     
      public static void retirar_saldo (){
-         
          System.out.println("---------RETIRAR SALDO-----------");
          System.out.println("Ingresa la cantidad a retirar: ");
          double cantidadRetirar = sc.nextDouble();
-         //validamos que tenga dinero y que no pida numeros negativos
          if (cantidadRetirar >0 && cantidadRetirar <=saldo){
-         saldo = saldo - cantidadRetirar;//restamos al saldo global 
-         System.out.println("Retiro exitoso");
-         System.out.println("Saldo restante :" + saldo);
-         
+             saldo = saldo - cantidadRetirar;
+             System.out.println("Retiro exitoso");
+             System.out.println("Saldo restante :" + saldo);
          }else if (cantidadRetirar > saldo ){
              System.out.println("error: saldo insuficiente. tu saldo actual es de : "+saldo );
-             
-             }else{
-                 System.out.println("cantidad no valida ");
-                 }
+         }else{
+             System.out.println("cantidad no valida ");
+         }
+     }
+
+     // CAMBIO 1: Nuevo método agregado para actualizar el saldo global al depositar
+     public static void depositar_dinero() {
+         System.out.println("---------DEPOSITAR DINERO-----------");
+         System.out.println("Ingresa la cantidad a depositar: ");
+         double cantidadDepositar = sc.nextDouble();
+         if (cantidadDepositar > 0) {
+             saldo = saldo + cantidadDepositar;
+             System.out.println("Depósito exitoso.");
+             System.out.println("Tu nuevo saldo es de: " + saldo);
+         } else {
+             System.out.println("Cantidad no válida.");
+         }
      }
 }
